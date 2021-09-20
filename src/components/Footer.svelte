4bcd45ba6c5d134c6d1d4b81e1d450a2578f4146
kailoon <chankailoon@gmail.com>
2021-09-20 12:16:35 +0800

    improve styling again...

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,22 @@
+<script lang="typescript">
+	const year = new Date().getFullYear();
+</script>
+
 <footer>
-	<p>Created by kailoon.</p>
+	<p>
+		{year}. Created by <a href="https://kailoon.com" title="visit site">kailoon</a>. View on
+		<a href="https://github.com/kailoon/sveltekit-auth-blog" title="view on github">Github</a>
+	</p>
 </footer>
 
-<style>
+<style lang="scss">
 	footer {
 		padding: 40px 20px;
 		font-size: 14px;
-		color: var(--gray-light);
+		color: #aaa;
+		a {
+			font-weight: 500;
+			color: #333;
+		}
 	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <section>
-	<h1><span>Write Less</span> Do More</h1>
+	<h1 class="rainbow-text">Write Less.<br /> Do More</h1>
 </section>
 
 <style lang="scss">
@@ -8,20 +8,36 @@
 		line-height: 1;
 		letter-spacing: -2px;
 		margin-bottom: 40px;
-		color: var(--linkColor);
-		span {
-			background: linear-gradient(60deg, #0cebeb, #20e3b2, #29ffc6);
-			-webkit-background-clip: text;
-			background-clip: text;
-			-webkit-text-fill-color: transparent;
-		}
+		font-weight: 800;
 	}
+	.rainbow-text {
+		background: #ca4246;
+		background-color: #ca4246;
+		background: conic-gradient(
+			#ca4246 20%,
+			#e16541 20%,
+			#e16541 40%,
+			#f18f43 40%,
+			#f18f43 60%,
+			#8b9862 60%,
+			#8b9862 80%,
+			#476098 80%,
+			#476098 93.333%,
+			#a7489b 93.333%
+		);
+
+		background-size: 60%;
+		background-repeat: repeat;
+
+		-webkit-background-clip: text;
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+
 	@media (min-width: 40em) {
 		h1 {
-			font-size: 140px;
-			span {
-				display: block;
-			}
+			font-size: 110px;
+			font-weight: 800;
 		}
 	}
 </style>
